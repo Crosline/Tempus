2564bbcb6ad3af95b0f61e8be8874672c70e4ed9
Crosline <cagkancaglayanel@gmail.com>
2023-08-09 17:56:27 +0300

    [A] One in all header file for games to use

---
+++ src/Tempus.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#include "Framework/API.h"
+#include "Framework/EngineConfig.h"
+
+#include "Core/Application.h"
+#include "Core/EntryPoint.h"
+
+/// Logger Subsystem ///
+#include "Subsystems/Logger/Logger.h"
+#include "Subsystems/Logger/Enums.h"
