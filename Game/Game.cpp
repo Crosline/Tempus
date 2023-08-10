6071e54698b7276e29ab766e9de74eae2dbcdbc4
Crosline <cagkancaglayanel@gmail.com>
2023-08-10 15:06:17 +0300

    [A][S] Add SharedLogger test to the game.cpp

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -12,11 +12,15 @@ public:
 std::unique_ptr<Tempus::Application> Tempus::CreateApplication() {
 	const Log::Logger log(Log::Category::General, "Application");
 
-	log.Debug("test enabled general", __func__);
+	log.Debug("test enabled general");
+
 	TLogger::DisableCategory(Log::Category::General);
+	TLogger::SharedLogger->Debug("Shared test for disabled");
 	log.Debug("test2 for disabled general", __func__);
-	// TLogger::EnableCategory(Log::Category::General);
-	// log.Debug("test3 enabled general", __func__);
+
+	TLogger::EnableCategory(Log::Category::General);
+	TLogger::SharedLogger->Debug("Shared test for enabled");
+	log.Debug("test3 enabled general");
 
 	return std::make_unique<Game>();
 }
