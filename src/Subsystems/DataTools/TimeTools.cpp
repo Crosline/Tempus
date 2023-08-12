047863e9c6c9ecbaa1450084faa0e5c856f2fc52
Crosline <cagkancaglayanel@gmail.com>
2023-08-12 14:10:13 +0300

    [A] Added TimeTools to support time related operations

---
+++ src/Subsystems/DataTools/TimeTools.cpp
@@ -0,0 +1,29 @@
+﻿#include "TimeTools.h"
+
+#include <iomanip>
+
+namespace Tempus {
+    namespace DataTools {
+        namespace TimeTools {
+            std::tm GetCurrentTime() {
+                std::tm currentTime{};
+                const time_t now = time(nullptr);
+                localtime_s(&currentTime, &now); // NOLINT(cert-err33-c)
+
+                return currentTime;
+            }
+
+            std::string GetCurrentTimeAsString() {
+                const std::tm currentTime = GetCurrentTime();
+
+                std::ostringstream buffer;
+                buffer << std::setfill('0') << std::setw(2) << currentTime.tm_hour;
+                buffer << ":" << std::setw(2) << currentTime.tm_min;
+                buffer << ":" << std::setw(2) << currentTime.tm_sec;
+
+                return buffer.str();
+            }
+
+        }
+    }
+}
---
+++ src/Subsystems/DataTools/TimeTools.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "API.h"
+
+#include <string>
+#include <sstream>
+
+#define T_TIME Tempus::DataTools::TimeTools
+
+namespace Tempus {
+
+    namespace DataTools {
+        namespace TimeTools {
+            TEMPUS_API std::tm GetCurrentTime();
+            TEMPUS_API std::string GetCurrentTimeAsString();
+            
+        }
+    }
+
+}
