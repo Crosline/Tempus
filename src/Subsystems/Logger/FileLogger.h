ab430cbc44ee74e5183dce2b88adc7ba6d8de405
Crosline <cagkancaglayanel@gmail.com>
2023-08-12 23:05:06 +0300

    [C][B] Disabled C4251 warnings for std usages in FileLogger

--- src/Subsystems/Logger/FileLogger.h
+++ src/Subsystems/Logger/FileLogger.h
@@ -7,7 +7,10 @@ namespace Tempus {
     namespace Log {
         class TEMPUS_API FileLogger final : public Logger {
         private:
+#pragma warning (push)
+#pragma warning (disable : 4251)
             mutable std::ofstream _outputFile;
+#pragma warning (pop)
         public:
             void Log(const Level& level, const std::string& message, const std::string& suffix) const override;
 
