85b5e838507f69f657a88a1368c7cb15ae97bcfc
Crosline <cagkancaglayanel@gmail.com>
2023-08-12 15:45:41 +0300

    [U][C] Separate Stringtools into two files

---
+++ src/Subsystems/DataTools/StringTools.cpp
@@ -0,0 +1,46 @@
+﻿#include "StringTools.h"
+
+#include <cstdarg>
+#include <stdexcept>
+
+#include "Common.h"
+
+namespace Tempus {
+    namespace DataTools {
+        namespace StringTools {
+
+            inline bool Contains(const char* message, const char* subtext) {
+                return strstr(message, subtext);
+            }
+
+            unsafe std::string Format(const char* message, ...) {
+                if (Contains(message, "%x")) {
+                    throw std::runtime_error("[ERROR] %x cannot be used with formatting.");
+                }
+
+                va_list args;
+                va_start(args, message);
+
+                const size_t size_s = std::vsnprintf(nullptr, 0, message, args) + 1; // Extra space for '\0'
+                if (size_s <= 1) {
+                    throw std::runtime_error("[ERROR] String size cannot be smaller than 1.");
+                }
+
+                const std::unique_ptr<char[]> buffer(new char[size_s]);
+
+                auto temp = std::vsnprintf(buffer.get(), size_s, message, args);
+
+                va_end(args);
+
+                std::string result = std::string(buffer.get(), buffer.get() + size_s - 1);
+
+                if (result.empty()) {
+                    throw std::runtime_error("[ERROR] Something went wrong while formatting.");
+                }
+
+                return result;
+            }
+
+        }
+    }
+}
--- src/Subsystems/DataTools/StringTools.h
+++ src/Subsystems/DataTools/StringTools.h
@@ -1,46 +1,20 @@
 ﻿#pragma once
-#include "Common.h"
+#include <string>
 
-#include <cstdarg>
-#include <stdexcept>
+#include "Common.h"
+#include "API.h"
+
+#define T_STRING Tempus::DataTools::StringTools
 
 namespace Tempus {
     namespace DataTools {
         namespace StringTools {
 
-            inline bool Contains(const char* message, const char* subtext) {
-                return strstr(message, subtext);
-            }
+            inline bool Contains(const char* message, const char* subtext);
 
             // inspired by
             // https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
-            unsafe std::string Format(const char* message, ...) {
-                if (Contains(message, "%x")) {
-                    throw std::runtime_error("[ERROR] %x cannot be used with formatting.");
-                }
-
-                va_list args;
-                va_start(args, message);
-
-                const size_t size_s = std::vsnprintf(nullptr, 0, message, args) + 1; // Extra space for '\0'
-                if (size_s <= 1) {
-                    throw std::runtime_error("[ERROR] String size cannot be smaller than 1.");
-                }
-
-                const std::unique_ptr<char[]> buffer(new char[size_s]);
-
-                auto temp = std::vsnprintf(buffer.get(), size_s, message, args);
-
-                va_end(args);
-
-                std::string result = std::string(buffer.get(), buffer.get() + size_s - 1);
-
-                if (result.empty()) {
-                    throw std::runtime_error("[ERROR] Something went wrong while formatting.");
-                }
-
-                return result;
-            }
+            TEMPUS_API unsafe std::string Format(const char* message, ...);
 
         }
     }
