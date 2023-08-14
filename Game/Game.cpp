6e3d8bcfa51cc6aa8467a7a397d2cf8239e013a3
Crosline <cagkancaglayanel@gmail.com>
2023-08-14 15:33:57 +0300

    Trivial

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,35 +1,35 @@
+#include <locale>
 #include <Tempus.h>
-#include <iostream>
 
 class Game : public Tempus::Application {
 public:
-	Game() = default;
+    Game() = default;
 
-	~Game() = default;
+    ~Game() = default;
 };
 
 
 std::unique_ptr<Tempus::Application> Tempus::CreateApplication() {
-	const Log::Logger* log = new Log::ConsoleLogger(Log::Category::General, "Application");
-	const std::shared_ptr<Log::Logger> log2 = std::make_shared<Log::FileLogger>(Log::Category::General, "Application", "game_log.txt");
+    const Log::Logger* log = new Log::ConsoleLogger(Log::Category::General, "Application");
+    const Log::Logger* log2 = new Log::FileLogger(Log::Category::General, "Application", "test.log");
 
-	log->Debug("test enabled general");
-	log2->Debug("test enabled general");
+    log->Debug("test enabled general");
+    log2->Debug("test enabled general");
 
-	
-	TLogger::DisableCategory(Log::Category::General);
-	log->Debug("test2 for disabled general", __func__);
 
-	TLogger::SharedLogger->Debug("Shared test for disabled");
-	TLogger::EnableCategory(Log::Category::General);
-	TLogger::SharedLogger->Warning("Shared test for enabled");
-	log->Debug("test3 enabled general");
+    TLogger::DisableCategory(Log::Category::General);
+    log->Debug("test2 for disabled general", __func__);
 
-	const std::string test = DataTools::StringTools::Format("a %d b", 5);
-	log->Debug(test);
-	log->Debug(DataTools::StringTools::Format("c %d", 5));
-	log->Debug(DataTools::StringTools::Format("dd"));
+    TLogger::SharedLogger->Debug("Shared test for disabled");
+    TLogger::EnableCategory(Log::Category::General);
+    TLogger::SharedLogger->Warning("Shared test for enabled");
+    log->Debug("test3 enabled general");
 
-	delete log;
-	return std::make_unique<Game>();
+    const std::string test = DataTools::StringTools::Format("a %d b", 5);
+    log->Debug(test);
+    log->Debug(DataTools::StringTools::Format("c %d", 5));
+    log->Debug(DataTools::StringTools::Format("dd"));
+
+    delete log;
+    return std::make_unique<Game>();
 }
